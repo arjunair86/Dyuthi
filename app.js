c5d8097b919bf41929b5927d6c839ab1bafd3fd3
Arjun <arjunair86@gmail.com>
2016-08-28 21:27:14 +0530

    json object response

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ app.get('/9a7e3672c6b44fdc6caee12b84572240', function(req, res){
 	var worksheet;
 	var sheet_name_list = workbook.SheetNames;
 	worksheet = workbook.Sheets[sheet_name_list[0]];
-	fullJson = xlsx.utils.sheet_to_json(worksheet);
-	res.send(xlsx.utils.sheet_to_json(worksheet));
+	dyuthi = xlsx.utils.sheet_to_json(worksheet);
+	var result = {dyuthi};
+	res.json(result);
 });
 
 app.listen(8000, function(){
