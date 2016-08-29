64327215391c4cf24991ea23458ae8e27e0a609f
Arjun <arjunair86@gmail.com>
2016-08-29 23:16:27 +0530

    added day1, day2, day3

--- routes/all.js
+++ routes/all.js
@@ -1,17 +1,52 @@
+'use strict';
+
 const express = require('express');
 const allRouter = express.Router();
 
 const xlsx = require('xlsx');
 const workbook = xlsx.readFile('sample2.xlsx');
 
+var sheet_name_list = workbook.SheetNames;
+var worksheet = workbook.Sheets[sheet_name_list[0]];
+var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+
 allRouter.route('/')
 	.get(function(req, res){
-	var worksheet;
-	var sheet_name_list = workbook.SheetNames;
-	worksheet = workbook.Sheets[sheet_name_list[0]];
-	dyuthi = xlsx.utils.sheet_to_json(worksheet);
-	var result = {dyuthi};
-	res.json(result);
+		var result = {dyuthi};
+		res.json(result);
+});
+
+allRouter.route('/day1')
+	.get(function(req, res){
+		var result = {}, i;
+		for (i = 0; i<dyuthi.length; i++){
+			if(dyuthi[i].Day === 'Day 1')
+				result['event_'+i] = dyuthi[i];
+		}
+		var result = {result};
+		res.json(result);
+});
+
+allRouter.route('/day2')
+	.get(function(req, res){
+		var result = {}, i;
+		for (i = 0; i<dyuthi.length; i++){
+			if(dyuthi[i].Day === 'Day 2')
+				result['event_'+i] = dyuthi[i];
+		}
+		var result = {result};
+		res.json(result);
+});
+
+allRouter.route('/day3')
+	.get(function(req, res){
+		var result = {}, i;
+		for (i = 0; i<dyuthi.length; i++){
+			if(dyuthi[i].Day === 'Day 3')
+				result['event_'+i] = dyuthi[i];
+		}
+		var result = {result};
+		res.json(result);
 });
 
 module.exports = allRouter;
