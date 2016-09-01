6b50c35c87f89f31f3fae8c3de4b5eddeec60c89
Arjun <arjunair86@gmail.com>
2016-09-01 21:38:16 +0530

    added 'addLinks' middleware

--- app.js
+++ app.js
@@ -2,14 +2,39 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 1050;
 const app = express();
+const fs = require('fs');
+
+const xlsx = require('xlsx');
+const workbook = xlsx.readFile('dyuthi16_schedule.xlsx');
+const sheet_name_list = workbook.SheetNames;
+const worksheet = workbook.Sheets[sheet_name_list[0]];
+const dyuthi = xlsx.utils.sheet_to_json(worksheet);
 
 app.use(bodyParser.urlencoded({ extended :true }));
 app.use(bodyParser.json());
 
 app.use('/public', express.static('public'));
 
+var addLinks = function(req, res, next){
+	for(var i=0; i<dyuthi.length; i++){
+		var eventType = dyuthi[i].Type;
+		var eventName = dyuthi[i].Event;
+		if(eventType !== "Expo"){
+			var files = fs.readdirSync(__dirname + '/public/imgs/' + eventType);
+			var ename = eventName.replace(/ /g, '').toLowerCase();
+				for (var j=0; j<files.length; j++){
+					var cleanString = files[j].split('-')[0].split('.')[0].split(' ')[0].toLowerCase(); 
+					if (ename.match(cleanString) != null ){
+						dyuthi[i].Link = req.get('host') + '/public/imgs/' + eventType + '/' + files[j];
+					}
+				}	
+			}
+	}
+	res.locals.dyuthi = dyuthi;
+	next();
+}
+app.use(addLinks);
+
 app.use('/all', require('./routes/all'));
 
-app.listen(port, function(){
-	console.log('Server listening to port', port);
-});
+app.listen(port);
--- routes/all.js
+++ routes/all.js
@@ -2,41 +2,17 @@
 
 const express = require('express');
 const allRouter = express.Router();
-const fs = require('fs');
-
-const xlsx = require('xlsx');
-const workbook = xlsx.readFile('dyuthi16_schedule.xlsx');
-
-const sheet_name_list = workbook.SheetNames;
-const worksheet = workbook.Sheets[sheet_name_list[0]];
-const dyuthi = xlsx.utils.sheet_to_json(worksheet);
-
-
-var i;
-for(i=0; i<dyuthi.length; i++){
-	var eventType = dyuthi[i].Type;
-	var eventName = dyuthi[i].Event;
-	if(eventType !== "Expo"){
-		var files = fs.readdirSync(__dirname + '/../public/imgs/' + eventType);
-		var ename = eventName.replace(/ /g, '').toLowerCase();
-			for (var j=0; j<files.length; j++){
-				var cleanString = files[j].split('-')[0].split('.')[0].split(' ')[0].toLowerCase(); 
-				if (ename.match(cleanString) != null ){
-					dyuthi[i].Link = 'localhost:1050/public/imgs/'+eventType+'/'+files[j];
-				}
-			}	
-		}
-}
 
 allRouter.route('/')
-	.get(function(req, res){		
+	.get(function(req, res){
+		var dyuthi = res.locals.dyuthi;		
 		var result = {dyuthi};
 		res.json(result);
 });
 
 allRouter.route('/day1')
 	.get(function(req, res){
-		var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+		var dyuthi = res.locals.dyuthi;
 		var i, result = [];
 		for (i = 0; i<dyuthi.length; i++){
 			if(dyuthi[i].Day === 'Day 1'){
@@ -49,7 +25,7 @@ allRouter.route('/day1')
 
 allRouter.route('/day2')
 	.get(function(req, res){
-		var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+		var dyuthi = res.locals.dyuthi;
 		var i, result = [];
 		for (i = 0; i<dyuthi.length; i++){
 			if(dyuthi[i].Day === 'Day 2'){
@@ -62,7 +38,7 @@ allRouter.route('/day2')
 
 allRouter.route('/day3')
 	.get(function(req, res){
-		var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+		var dyuthi = res.locals.dyuthi;
 		var i, result = [];
 		for (i = 0; i<dyuthi.length; i++){
 			if(dyuthi[i].Day === 'Day 3'){
@@ -75,7 +51,7 @@ allRouter.route('/day3')
 
 allRouter.route('/upcoming')
 	.get(function(req, res){
-		var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+		var dyuthi = res.locals.dyuthi;
 		var result = [], i;
 		for (i = 0; i<dyuthi.length; i++){
 			if(dyuthi[i].Status === 'Upcoming'){
@@ -88,7 +64,7 @@ allRouter.route('/upcoming')
 
 allRouter.route('/ongoing')
 	.get(function(req, res){
-		var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+		var dyuthi = res.locals.dyuthi;
 		var result = [], i;
 		for (i = 0; i<dyuthi.length; i++){
 			if(dyuthi[i].Status === 'Ongoing'){
@@ -105,24 +81,12 @@ allRouter.route('/:eventName')
 		var eventName = req.params.eventName;
 		var eventType;
 
-		var dyuthi = xlsx.utils.sheet_to_json(worksheet);
+		var dyuthi = res.locals.dyuthi;
 		var result = [], i;
 		for (i = 0; i<dyuthi.length; i++){
 			if(dyuthi[i].Event === eventName){
 				result.push(dyuthi[i]);
 				eventType = dyuthi[i].Type;
-			}
-		}
-
-		var files = fs.readdirSync(__dirname + '/../public/imgs/' + eventType);
-		var ename = eventName.replace(/ /g, '').toLowerCase();
-
-
-		for (i=0; i<files.length; i++){
-			var cleanString = files[i].split('-')[0].split('.')[0].toLowerCase(); 
-			if (ename.match(cleanString) != null ){
-				console.log(result[0]);
-				result[0].Link = req.get('host')+'/public/imgs/'+eventType+'/'+files[i];
 			}
 		}
 
